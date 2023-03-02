e6d38810bd541205a4e90f3db4b50bf90748a029
Drew Alleman <52759042+Drew-Alleman@users.noreply.github.com>
2023-03-01 21:02:51 -0700

    Now can update using installer

--- install/install.sh
+++ install/install.sh
@@ -1,5 +1,8 @@
+sudo rm /usr/local/bin/ds 2>&1
+sudo rm -rf DataSurgeon 2>&1
 git clone https://github.com/Drew-Alleman/DataSurgeon 
 cd DataSurgeon
 cargo build --release
 chmod +x target/release/ds
 sudo mv target/release/ds /usr/local/bin/ds
+sudo rm -rf DataSurgeon 2>&1
