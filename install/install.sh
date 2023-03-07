a93663d0d06ae955a5d11f05f89cbca366adc5eb
Drew <52759042+Drew-Alleman@users.noreply.github.com>
2023-03-07 09:12:45 -0700

    Removed `sudo` for rm

--- install/install.sh
+++ install/install.sh
@@ -1,9 +1,9 @@
 sudo rm /usr/local/bin/ds 2>&1
-sudo rm -rf DataSurgeon 2>&1
+rm -rf DataSurgeon 2>&1
 git clone https://github.com/Drew-Alleman/DataSurgeon 
 cd DataSurgeon
 cargo build --release
 chmod +x target/release/ds
 sudo mv target/release/ds /usr/local/bin/ds
 cd ..
-sudo rm -rf DataSurgeon 2>&1
+rm -rf DataSurgeon 2>&1
