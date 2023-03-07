fb5b11a37fb0c8430718ea88a223ad63f47b3d9b
Drew <52759042+Drew-Alleman@users.noreply.github.com>
2023-03-06 21:07:28 -0800

    Updated the URL Regex to Return Less False Positives

--- src/main.rs
+++ src/main.rs
@@ -207,7 +207,7 @@ impl  DataSurgeon {
         let regex_map: HashMap<&str, Regex> = [
             ("credit_card", Regex::new(r"\b(\d{4}[- ]?\d{4}[- ]?\d{4}[- ]?\d{4})\b").unwrap()),
             ("email", Regex::new(r"\b([A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4})\b").unwrap()),
-            ("url", Regex::new(r"((?:https?|ftp)://[^\s/$.?#].[^\s]*)").unwrap()),
+            ("url", Regex::new(r#"(https?://(?:[^\s.,;:"'<>()\[\]{}]+\.)*[^\s.,;:"'<>()\[\]{}]+(/[^\s]*[^\s.,;:"'<>()\[\]{}\s])?)"#).unwrap()),
             ("ip_address", Regex::new(r"\b((?:\d{1,3}\.){3}\d{1,3})\b").unwrap()),
             ("social_security", Regex::new(r"\b(\d{3}-\d{2}-\d{4})\b").unwrap()),
             ("ipv6_address", Regex::new(r"([0-9a-fA-F]{1,4}(:[0-9a-fA-F]{1,4}){7})").unwrap()),
