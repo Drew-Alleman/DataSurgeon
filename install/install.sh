cf8763e60a7c1b4a9478c97a5e1a2a77d628a444
Drew <52759042+Drew-Alleman@users.noreply.github.com>
2023-03-09 12:32:16 -0700

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
-
 if [ -d "DataSurgeon" ]; then
     echo "[*] Removing old 'DataSurgeon' directory"
     rm -rf DataSurgeon 2>&1
 fi
-
+if [ -f "/usr/local/bin/ds" ]; then
+    echo "[*] Removing old executable (password required)"
+    rm -rf /usr/local/bin/ds 2>&1
+fi
 git clone https://github.com/Drew-Alleman/DataSurgeon
 cd DataSurgeon
 cargo build --release
-if [ -f "$(pwd)/target/release/ds" ]; then
-   chmod +x target/release/ds
-    read -p "[!] DataSurgeon was succesfully compiled! Would you like to add it to your path? (requires password) [y/n]: " yn
-    case $yn in
-        [Yy]* ) sudo mv target/release/ds /usr/local/bin/ds;;
-        [Nn]* ) echo "Done! ds can be found: /target/release/";;
-        * ) echo "Please answer y or n.";;
-    esac
-    rm -rf DataSurgeon 2>&1
-fi
+chmod +x target/release/ds
+sudo mv target/release/ds /usr/local/bin/ds
+cd ..
+rm -rf DataSurgeon 2>&1
