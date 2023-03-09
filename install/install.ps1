d5c7a8fab49c756d04c10ccbb58a6386ded1fbca
Drew <52759042+Drew-Alleman@users.noreply.github.com>
2023-03-09 13:10:22 -0700

    Updated Installer to add more Verbosity
    See https://github.com/Drew-Alleman/DataSurgeon/issues/3

--- install/install.ps1
+++ install/install.ps1
@@ -2,21 +2,23 @@ $executablePath = "C:/ds/ds.exe";
 if ((Test-Path -Path $executablePath -PathType Leaf)) {
   Write-Host "[*] Removing the previously installed version"
   Remove-Item -Path $executablePath -Force -ErrorAction SilentlyContinue | Out-Null
-
 }
 
 if ((Test-Path -Path "DataSurgeon" -PathType Leaf)) {
   Write-Host "[*] Removing the old 'DataSurgeon' directory"
   Remove-Item -Path "DataSurgeon" -Force -ErrorAction SilentlyContinue | Out-Null
 }
-Write-Host -Message "[*] Downloading DataSurgeon from Github..."
+
+Write-Host "[*] Downloading DataSurgeon from Github..."
 git clone https://github.com/Drew-Alleman/DataSurgeon/ --quiet
 cd DataSurgeon
 cargo build --release 
-if (-not(Test-Path -Path "C:/ds/" -PathType Leaf)) {
+
+if (!(Test-Path -Path $executablePath -PathType Leaf)) {
   Write-Host "[*] Creating C:/ds/ to store the executable"
   mkdir C:/ds/ | Out-Null
 }
+
 Write-Host "[*] Binding ds.exe to path (requires admin)"
 copy "$(Get-Location)\target\release\ds.exe" $executablePath
 setx PATH "$env:PATH;C:/ds/"
