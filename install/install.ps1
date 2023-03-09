04874fb5f614756c1440cf2091189635067a5f4c
Drew <52759042+Drew-Alleman@users.noreply.github.com>
2023-03-09 13:00:09 -0700

    Update install.ps1

--- install/install.ps1
+++ install/install.ps1
@@ -1,12 +1,12 @@
 $executablePath = "C:/ds/ds.exe";
 if ((Test-Path -Path $executablePath -PathType Leaf)) {
-  Write-Host -Message "[*] Removing the previously installed version"
+  Write-Host "[*] Removing the previously installed version"
   Remove-Item -Path $executablePath -Force -ErrorAction SilentlyContinue | Out-Null
 
 }
 
 if ((Test-Path -Path "DataSurgeon" -PathType Leaf)) {
-  Write-Host -Message "[*] Removing the old 'DataSurgeon' directory"
+  Write-Host "[*] Removing the old 'DataSurgeon' directory"
   Remove-Item -Path "DataSurgeon" -Force -ErrorAction SilentlyContinue | Out-Null
 }
 Write-Host -Message "[*] Downloading DataSurgeon from Github..."
@@ -14,9 +14,9 @@ git clone https://github.com/Drew-Alleman/DataSurgeon/ --quiet
 cd DataSurgeon
 cargo build --release 
 if (-not(Test-Path -Path "C:/ds/" -PathType Leaf)) {
-  Write-Host -Message "[*] Creating C:/ds/ to store the executable"
+  Write-Host "[*] Creating C:/ds/ to store the executable"
   mkdir C:/ds/ | Out-Null
 }
-Write-Host -Message "[*] Binding ds.exe to path (requires admin)"
+Write-Host "[*] Binding ds.exe to path (requires admin)"
 copy "$(Get-Location)\target\release\ds.exe" "C:/ds/ds.exe"
 setx PATH "$env:PATH;C:/ds/"
