aa759c937e5d6fe464a538be4a5ec3f3ff298e40
Drew <52759042+Drew-Alleman@users.noreply.github.com>
2023-03-09 12:34:39 -0700

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -5,12 +5,16 @@ if [ -d "DataSurgeon" ]; then
 fi
 if [ -f "/usr/local/bin/ds" ]; then
     echo "[*] Removing old executable (password required)"
-    rm -rf /usr/local/bin/ds 2>&1
+    sudo rm -rf /usr/local/bin/ds 2>&1
 fi
 git clone https://github.com/Drew-Alleman/DataSurgeon
 cd DataSurgeon
 cargo build --release
-chmod +x target/release/ds
-sudo mv target/release/ds /usr/local/bin/ds
-cd ..
-rm -rf DataSurgeon 2>&1
+if [ -f "target/release/ds" ]; then
+    echo "[*] Removing old executable (password required)"
+    chmod +x target/release/ds
+    sudo mv target/release/ds /usr/local/bin/ds
+    cd ..
+    rm -rf DataSurgeon 2>&1
+    exit
+fi
