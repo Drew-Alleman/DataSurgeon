28a411021c340501fe916423d5ed42e794307904
Drew <52759042+Drew-Alleman@users.noreply.github.com>
2023-07-22 22:57:59 -0700

    Added Y/N prompt for adding ds to path

--- install/install.sh
+++ install/install.sh
@@ -1,31 +1,60 @@
 #!/bin/bash
-echo "[*] Updating OS and Installing OpenSSL libraries.."
-sudo apt update
-sudo apt upgrade
-sudo apt install pkg-config libssl-dev
+
+# Sleep to let the web request finish
+sleep 1
+# Update OS and install OpenSSL libraries
+echo "[*] Updating OS and installing OpenSSL libraries... (password required)"
+sudo apt update && sudo apt upgrade && sudo apt install pkg-config libssl-dev
+
+# Check and remove existing DataSurgeon directory if it exists
 if [ -d "DataSurgeon" ]; then
-    echo "[*] Removing old 'DataSurgeon' directory"
-    rm -rf DataSurgeon 2>&1
+    echo "[*] Removing old 'DataSurgeon' directory..."
+    rm -rf DataSurgeon
 fi
+
+# Check and remove existing 'ds' executable if it exists
 if [ -f "/usr/local/bin/ds" ]; then
-    echo "[*] Removing old executable '/usr/local/bin/ds' (password required)"
-    sudo rm -rf /usr/local/bin/ds 2>&1
+    echo "[*] Removing old 'ds' executable..."
+    sudo rm /usr/local/bin/ds
 fi
-echo "[*] Downloading DataSurgeon's source from Github...."
-if ! git clone https://github.com/Drew-Alleman/DataSurgeon --quiet; then
-    echo "[*] Failed to download DataSurgeon from github"
+
+# Clone DataSurgeon's source from Github
+echo "[*] Downloading DataSurgeon's source from Github..."
+if git clone https://github.com/Drew-Alleman/DataSurgeon; then
+    cd DataSurgeon
+    # Build the project
+    cargo build --release
+else
+    echo "[!] Failed to download DataSurgeon from Github"
     exit 1
 fi
-cd DataSurgeon
-cargo build --release
+
+# Check if the build succeeded
 if [ -f "target/release/ds" ]; then
-    echo "[*] Adding ds to your local bin (password required)"
-    chmod +x target/release/ds
-    sudo mv target/release/ds /usr/local/bin/ds
-    echo "[*] Creating directory ~/.DataSurgeon/ and moving plugins.json into it"
-    mkdir -p ~/.DataSurgeon
-    mv plugins.json ~/.DataSurgeon/
-    cd ..
-    rm -rf DataSurgeon 2>&1
-    exit
+    echo "Would you like to add 'ds' to your local bin directory? This allows you to run the 'ds' command from any directory in your terminal. [Y/n]"
+    read -r response
+    if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]; then
+        echo "[*] Adding 'ds' to your local bin..."
+        chmod +x target/release/ds
+        sudo mv target/release/ds /usr/local/bin/
+    else
+        echo "Skipped adding 'ds' to local bin."
+    fi
+else
+    echo "[!] Build failed. The 'ds' executable does not exist."
+    exit 1
 fi
+
+
+# Check for existing plugins.json and only move the new file if it doesn't exist
+mkdir -p ~/.DataSurgeon
+if [ ! -f ~/.DataSurgeon/plugins.json ]; then
+    mv plugins.json ~/.DataSurgeon/
+    echo "[*] Moved plugins.json to ~/.DataSurgeon/"
+else
+    echo "[*] Existing plugins.json found, leaving it intact."
+fi
+
+# Clean up
+cd ..
+rm -rf DataSurgeon
